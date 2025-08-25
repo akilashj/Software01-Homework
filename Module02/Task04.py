037e1eb865d245002fe38e82cbb96329877dc190
akilashj <shashiduniakila98@outlook.com>
2025-08-25 12:50:42 +0530

    I have completed Task04 of Module 02.

---
+++ Module02/Task04.py
@@ -0,0 +1,15 @@
+Number1 = input("Enter the First Integer:")
+Number2 = input("Enter the Second Integer:")
+Number3 = input("Enter the Third Integer:")
+
+Num1 = int(Number1)
+Num2 = int(Number2)
+Num3 = int(Number3)
+
+Sum = (Num1 + Num2 + Num3)
+Product = (Num1 * Num2 * Num3)
+Average = (Sum / 3)
+
+print(f"The Sum is :",{Sum} )
+print(f"The Product is:",{Product})
+print(f"The Average is:",{Average})
