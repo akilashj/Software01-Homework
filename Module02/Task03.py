058c48ec7399e614e46a360bab5c32114fdac557
akilashj <shashiduniakila98@outlook.com>
2025-08-25 12:32:23 +0530

    I have completed Task03 of Module 02.

---
+++ Module02/Task03.py
@@ -0,0 +1,12 @@
+import math
+Length = input ("Please Enter the Length of the Rectangle: ")
+Width = input ("Please Enter the Width of the Rectangle: ")
+
+length = float (Length)
+width = float (Width)
+
+perimeter = 2 * (length + width)
+area = length * width
+
+print(f"The Perimeter of the Rectangle is: ", {perimeter})
+print(f"The Area of the Rectangle is: ",{area})
