e1343a6e37300bd4b9a3687f0121fca5c744305c
akilashj <shashiduniakila98@outlook.com>
2025-08-25 14:14:01 +0530

    I have completed Task06 of Module 02.

---
+++ Module02/Task06.py
@@ -0,0 +1,13 @@
+import random
+Random1 = random.randint(0, 9)
+Random2 = random.randint(0, 9)
+Random3 = random.randint(0, 9)
+
+print(f"{Random1}{Random2}{Random3}")
+
+Random01 = random.randint(1, 6)
+Random02 = random.randint(1, 6)
+Random03 = random.randint(1, 6)
+Random04 = random.randint(1, 6)
+
+print(f"{Random01}{Random02}{Random03}{Random04}")
